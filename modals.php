739e4622e0e0002bd5d719a0285a4d0d48eddcab
flor1211 <florenciomendozarosalesjr@gmail.com>
2025-04-05 11:51:28 +0800

    Add login logout

---
+++ modals.php
@@ -0,0 +1,18 @@
+
+<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title" id="logoutModalLabel">Confirm Logout</h5>
+         </div>
+      <div class="modal-body">
+          Are you sure you want to logout?
+      </div>
+      <div class="modal-footer">
+         <a href="../logout.php" class="btn btn-danger">Logout</a>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+
+      </div>
+    </div>
+  </div>
+</div>
