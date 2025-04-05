f436db660c299076cc452eaa8eed9e66f1bd162e
flor1211 <florenciomendozarosalesjr@gmail.com>
2025-04-05 21:36:09 +0800

    Add booking folder wiht its modules

---
+++ booking/confirmation.php
@@ -0,0 +1,79 @@
+<?php
+
+  session_start();
+
+  if (!isset($_SESSION['payments_completed'])) {
+    header('Location: payments.php');
+    exit();
+  } 
+
+  session_unset();
+  session_destroy();
+  
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Bootstap S icons CDN-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <title>JetSetGo</title>
+
+    <link rel="stylesheet" href="booking-style.css">
+
+  </head>
+    <body style="margin: 0;">
+         <!-- NavBar Container -->
+         <div id="navbar-container">
+            <script>
+                fetch("topbar.php")
+                  .then(res => res.text())
+                  .then(data => {
+                    document.getElementById("navbar-container").innerHTML = data;
+                  });
+              </script>
+        </div>
+
+
+        <!-- Main Content -->
+        <div style="padding: 20px;">
+            <h1>JeSetGo</h1>
+    
+
+        </div>
+
+        <!-- Steps Container -->
+        <div id="steps-container">
+            <script>
+                const currentPage = location.pathname.split("/").pop();
+                fetch("stepsbar.php?page=" + currentPage)
+                    .then(res => res.text())
+                    .then(data => {
+                        document.getElementById("steps-container").innerHTML = data;
+                    });
+            </script>
+        </div>
+
+        <!-- Main Container -->
+        <div>
+            <a href="/initial/login.php" class="btn btn-primary">
+              <i class="bi bi-airplane"></i> Back to Login
+            </a>
+        </div>
+
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script src="booking.js"></script>
+
+  </body>
+</html>
