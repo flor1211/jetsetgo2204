6e0db7a88f7a4feee4d9c43c56990d9dcfbd2d59
VonHinanay <vhinanay@gmail.com>
2025-04-10 20:57:27 +0800

    updatedddd

--- newflights.php
+++ newflights.php
@@ -60,9 +60,9 @@
                   <td>30</td>
                   <td>Pending</td>
                   <td>
-                    <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
-                    <button class="btn btn-primary"><i class="bi bi-pencil-square"></i></button>
-                    <button class="btn btn-danger"><i class="bi bi-trash"></i></button>
+                    <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#readData">VIEW<i class="bi bi-eye"></i></button>
+                    <button class="btn btn-primary"><i class="bi bi-pencil-square">EDIT</i></button>
+                    <button class="btn btn-danger"><i class="bi bi-trash">DELETE</i></button>
                   </td>
                 </tr>
               </tbody>
